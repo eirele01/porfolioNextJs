7d8050d12118c599be820cc6bf4e42a6c45f1993
eirele01 <38448465+eirele01@users.noreply.github.com>
2024-04-17 11:17:23 +0800

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           {/* logo */}
           <Link href="/">
             <Image
-              src="./logo.svg"
+              src="/porfolioNextJs/logo.svg"
               alt="logo"
               width={220}
               height={48}
