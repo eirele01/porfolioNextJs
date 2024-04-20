25bb81340c88cba319b262eac65196de04dffbb9
eirele01 <38448465+eirele01@users.noreply.github.com>
2024-04-20 12:13:36 +0800

    Update Socials.jsx

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -37,7 +37,7 @@ export const socialData = [
   },
   {
     name: "Github",
-    link: "https://github.com/eirele01/porfolioNextJs",
+    link: "https://github.com",
     Icon: RiGithubLine,
   },
 ];
