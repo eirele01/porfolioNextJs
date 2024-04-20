13188de5cff15ddef9860d5434645b86d0cdb167
eirele01 <38448465+eirele01@users.noreply.github.com>
2024-04-20 13:35:11 +0800

    Update index.jsx

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -14,8 +14,7 @@ const Contact = () => {
     const myForm = event.target;
     const formData = new FormData(myForm);
 
-    fetch("/", {
-      action: "https://getform.io/f/lbkmpxpb",
+    fetch("https://getform.io/f/lbkmpxpb", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
